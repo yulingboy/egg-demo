82921cb4958788676402ff167dc16368520442e6
yulingboy <yulinglsy@163.com>
2022-01-15 13:40:44 +0800

    修改CI配置

--- app/contract/request/user.js
+++ app/contract/request/user.js
@@ -1,20 +1,32 @@
 'use strict';
 module.exports = {
-  register: {
-    username: {
-      type: 'string',
-      description: '用户名',
-      example: 'qwe',
-    },
-    password: {
-      type: 'string',
-      description: '密码',
-      example: '123456',
-    },
-    email: {
-      type: 'string',
-      description: '邮箱',
-      example: 'yuling@qq.com',
-    },
-  },
+  	register: {
+    	username: {
+      		type: 'string',
+      		description: '用户名',
+      		example: 'qwe',
+    	},
+    	password: {
+    	  	type: 'string',
+    	  	description: '密码',
+    	  	example: '123456',
+    	},
+    	email: {
+    	  	type: 'string',
+    	  	description: '邮箱',
+    	  	example: 'yuling@qq.com',
+    	},
+	},
+	login: {
+		password: {
+		  	type: 'string',
+		  	description: '密码',
+		  	example: '123456',
+		},
+		email: {
+		  	type: 'string',
+		  	description: '邮箱',
+		  	example: 'yuling@qq.com',
+		},
+	},
 };
--- app/router.js
+++ app/router.js
@@ -4,11 +4,12 @@
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
-  const { router, controller } = app;
-  router.redirect('/api', '/swagger-ui.html', 302);
-  router.get('/', controller.home.test);
-  router.post('/api/user/register', controller.user.register);
-  router.post('/api/user/login', controller.user.login);
-  router.post('/api/email/sendEmail', controller.email.sendEmail);
-//   router.get('/api/email/checkCode', controller.email.checkCode);
+	const { router, controller } = app;
+	router.redirect('/api', '/swagger-ui.html', 302);
+	router.get('/', controller.home.test);
+	router.post('/api/user/register', controller.user.register);
+	router.post('/api/user/login', controller.user.login);
+	router.get('/api/user/:id', controller.user.show);
+	router.post('/api/email/sendEmail', controller.email.sendEmail);
+	//   router.get('/api/email/checkCode', controller.email.checkCode);
 };
